8719fb8a03be8af9bc8d7f0786aac48e7b3561cb
Benjamin M. Singleton <bsinglet@vt.edu>
2022-02-06 22:57:09 -0500

    Changed day 2 to use an Enum instead of a bunch of constants. This is cleaner and avoids the syntax issues I had with the consts.

--- aoc_2021_rust/src/day02.rs
+++ aoc_2021_rust/src/day02.rs
@@ -1,26 +1,29 @@
 use std::fs;
 
-const DIRECTION_UP: i32      = 0;
-const DIRECTION_DOWN: i32    = 1;
-const DIRECTION_FORWARD: i32 = 2;
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Down,
+    Forward
+}
 
-fn read_lines(filename: &str) -> Vec<(i32, i32)> {
-    let mut result: Vec<(i32, i32)> = Vec::new();
+fn read_lines(filename: &str) -> Vec<(Direction, i32)> {
+    let mut result: Vec<(Direction, i32)> = Vec::new();
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file.");
     let lines: Vec<&str> = contents.split_terminator("\n").collect();
     for each_pair in lines {
-        let (mut x, mut y) = (0, 0);
+        let (mut x, mut y) = (Direction::Up, 0);
         for (index, value) in each_pair.split_whitespace().enumerate()  {
             match index {
                 0 => {
                     x = match value {
-                        "forward" => DIRECTION_FORWARD,
-                        "down" => DIRECTION_DOWN,
-                        "up" => DIRECTION_UP,
+                        "forward" => Direction::Forward,
+                        "down" => Direction::Down,
+                        "up" => Direction::Up,
                         _ => {
                             println!("Not sure what to do with direction {}", value);
-                            -1
+                            Direction::Up
                         },
                     };
                 },
@@ -38,40 +41,40 @@ fn read_lines(filename: &str) -> Vec<(i32, i32)> {
     result
 }
 
-fn process_lines(result: &Vec<(i32, i32)>) -> i32 {
+fn process_lines(result: &Vec<(Direction, i32)>) -> i32 {
     let mut x = 0;
     let mut y = 0;
     for (direction, magnitude) in result {
         match direction {
-            0 => y -= magnitude, // up
-            1 => y += magnitude, // down
-            2 => x += magnitude, // forward
-            _ => println!("Don't recognize direction {}", direction),
+            Direction::Up => y -= magnitude,
+            Direction::Down => y += magnitude,
+            Direction::Forward => x += magnitude,
+            _ => println!("Don't recognize direction {:?}", direction),
         };
     }
     x * y
 }
 
-fn process_lines_part_two(result: &Vec<(i32, i32)>) -> i32 {
+fn process_lines_part_two(result: &Vec<(Direction, i32)>) -> i32 {
     let mut x = 0;
     let mut y = 0;
     let mut aim = 0;
     for (direction, magnitude) in result {
         match direction {
-            0 => aim -= magnitude, // up
-            1 => aim += magnitude, // down
-            2 => {                 // forward
+            Direction::Up => aim -= magnitude,
+            Direction::Down => aim += magnitude,
+            Direction::Forward => {
                 x += magnitude;
                 y += aim * magnitude;
             },
-            _ => println!("Don't recognize direction {}", direction),
+            _ => println!("Don't recognize direction {:?}", direction),
         };
     }
     x * y
 }
 
 pub fn main() {
-    let result = read_lines("input.txt");
+    let result = read_lines("day02_input.txt");
     println!("Part 1 - The final product of x and y is: {}", process_lines(&result));
     println!("Part 2 - The final product of x and y is: {}", process_lines_part_two(&result));
 }
