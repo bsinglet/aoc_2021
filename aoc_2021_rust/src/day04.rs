7e85e70933847475b5fdcbc8fe88da5e662eeb9e
Benjamin M. Singleton <bsinglet@vt.edu>
2022-02-10 20:50:36 -0500

    Fixed some of the bugs.

--- aoc_2021_rust/src/day04.rs
+++ aoc_2021_rust/src/day04.rs
@@ -37,44 +37,44 @@ fn find_winners(marked_boards: &Vec<[[bool;5];5]>) -> Vec<i32> {
     let mut winners: Vec<i32> = Vec::new();
     for (board_number, each_board) in marked_boards.iter().enumerate() {
         // check rows, columns, and diagonals for any five trues in a row
-        for each_row in 0..each_board.len() {
-            if all(each_row) {
-                winners.push(board_number);
+        for each_row in each_board {
+            if each_row.iter().all(|&x| x == true) {
+                winners.push(board_number as i32);
                 break;
             }
         }
 
         for each_column in 0..each_board[0].len() {
-            let mut this_column = (each_board[0][each_column],
+            let mut this_column = [each_board[0][each_column],
                                    each_board[1][each_column],
                                    each_board[2][each_column],
                                    each_board[3][each_column],
-                                   each_board[4][each_column]);
-            if all(this_column) {
-                winners.push(board_number);
+                                   each_board[4][each_column]];
+            if this_column.iter().all(|&x| x == true) {
+                winners.push(board_number as i32);
                 break;
             }
         }
 
         // check upper-left to lower-right diagnoal
-        let mut this_diagonal = (each_board[0][0],
+        let mut this_diagonal = [each_board[0][0],
                                  each_board[1][1],
                                  each_board[2][2],
                                  each_board[3][3],
-                                 each_board[4][4]);
-        if all(this_diagonal) {
-            winners.push(board_number);
+                                 each_board[4][4]];
+        if this_diagonal.iter().all(|&x| x == true) {
+            winners.push(board_number as i32);
             break;
         }
 
         // check lower-left to upper-right diagnoal
-        this_diagonal = (each_board[4][0],
+        this_diagonal = [each_board[4][0],
                                  each_board[3][1],
                                  each_board[2][2],
                                  each_board[1][3],
-                                 each_board[0][4]);
-        if all(this_diagonal) {
-            winners.push(board_number);
+                                 each_board[0][4]];
+        if this_diagonal.iter().all(|&x| x == true) {
+            winners.push(board_number as i32);
             break;
         }
     }
@@ -114,6 +114,7 @@ fn process_lines(numbers: &Vec<i32>, boards: &Vec<[[i32;5];5]>) -> i32 {
     }
 
     println!("After {} moves, winning board is number {}", numbers_called, winners[0]);
+    5
 }
 
 pub fn main() {
